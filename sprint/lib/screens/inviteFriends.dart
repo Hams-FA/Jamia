fde6839c5cbbf395923119040fd37525349711e1
AfnanSD <99503463+AfnanSD@users.noreply.github.com>
2022-09-30 12:18:53 +0300

    show toast message when invite button clicked
    + change button size

--- sprint/lib/screens/inviteFriends.dart
+++ sprint/lib/screens/inviteFriends.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:sprint/screens/user_model.dart';
 import 'package:sprint/screens/firestore_helper.dart';
 import 'package:sprint/firebase_options.dart';
@@ -56,11 +57,10 @@ class _inviteFriendsState extends State<inviteFriends> {
   List<String> selectedEmails = List<String>.empty(growable: true);
   List<dynamic> invitedAlready = List<dynamic>.empty(growable: true);
 
-  var jamiaId; //change this to id coming from selected jamia
+  var jamiaId;
   var len; //delete this? no need to check for max here
   bool allinvited = true;
 
-  //need to update this User
   final _auth = FirebaseAuth.instance;
   late User signedInUser;
 
@@ -139,8 +139,6 @@ class _inviteFriendsState extends State<inviteFriends> {
                                   itemCount: userData!.length,
                                   itemBuilder: (context, index) {
                                     final singleUser = userData[index];
-                                    //print('email friends users');
-                                    //print(singleUser.Email);
                                     if (!invitedAlready
                                         .contains(singleUser.Email)) {
                                       allinvited = false;
@@ -201,113 +199,65 @@ class _inviteFriendsState extends State<inviteFriends> {
                                     }
                                   }),
                             ),
-                            ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor:
-                                      Color.fromARGB(255, 76, 175, 80),
-                                ),
-                                onPressed: () {
-                                  //print('on pressed');
-                                  //print(selectedEmails.length); //
-                                  //if (selectedEmails.length > widget.data['maxMembers']) {
-                                  selectedEmails.forEach((element) async {
-                                    final docInviteFriends = FirebaseFirestore
-                                        .instance
-                                        .collection('JamiaGroup')
-                                        .doc(jamiaId)
-                                        .collection('members');
-                                    /*.doc(element);
-                      print('number');
-                      //int len = await docInviteFriends.snapshots().length;
-                      print(len);
-                      print(selectedEmails.length);*/
-                                    docInviteFriends
-                                        .doc(element)
-                                        .set({'status': 'pending'});
-                                    FirebaseFirestore.instance
-                                        .collection('tokens')
-                                        .where('userID', isEqualTo: element)
-                                        //.limit(1)
-                                        .get()
-                                        .then((value) {
-                                      final tokens = value.docs;
-                                      tokens.forEach((e) {
-                                        sendPushMessage(
-                                            'انقر لمراجعتها الان',
-                                            '!لقد تمت دعوتك لجمعية جديدة',
-                                            e.get('token'));
+                            SizedBox(
+                              height: 42, //height of button
+                              width: 200,
+                              //height: 42,
+                              //                  width: double.infinity,
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        Color.fromARGB(255, 76, 175, 80),
+                                  ),
+                                  onPressed: () {
+                                    selectedEmails.forEach((element) async {
+                                      final docInviteFriends = FirebaseFirestore
+                                          .instance
+                                          .collection('JamiaGroup')
+                                          .doc(jamiaId)
+                                          .collection('members');
+                                      docInviteFriends
+                                          .doc(element)
+                                          .set({'status': 'pending'});
+                                      FirebaseFirestore.instance
+                                          .collection('tokens')
+                                          .where('userID', isEqualTo: element)
+                                          //.limit(1)
+                                          .get()
+                                          .then((value) {
+                                        final tokens = value.docs;
+                                        tokens.forEach((e) {
+                                          sendPushMessage(
+                                              'انقر لمراجعتها الان',
+                                              '!لقد تمت دعوتك لجمعية جديدة',
+                                              e.get('token'));
+                                        });
+                                      });
+
+                                      final docSnapshot =
+                                          await FirebaseFirestore.instance
+                                              .collection("requestList")
+                                              .add({
+                                        'email': element,
+                                        'jamiaID': jamiaId
                                       });
                                     });
+                                    Navigator.pushNamed(context, '/home');
+                                    //Navigator.pop(context);
+                                    //}
 
-                                    /*final updateRequestList = FirebaseFirestore.instance
-                          .collection('requestList')
-                          .doc(element);*/
-
-                                    final docSnapshot = await FirebaseFirestore
-                                        .instance
-                                        .collection("requestList")
-                                        .add({
-                                      'email': element,
-                                      'jamiaID': jamiaId
-                                    });
-                                  });
-
-                                  /*if (docSnapshot.exists) {
-                        updateRequestList.update({jamiaId: true});
-                      } else {
-                        updateRequestList.set({jamiaId: true});
-                      }
-                    });*/
-                                  Navigator.pushNamed(context, '/home');
-                                  //Navigator.pop(context);
-                                  //}
-                                  /*
-                    else {
-                      showAlertDialog(BuildContext context) {
-                        // set up the button
-                        Widget okButton = TextButton(
-                          child: Text("OK"),
-                          onPressed: () {},
-                        );
-
-                        // set up the AlertDialog
-                        AlertDialog alert = AlertDialog(
-                          title: Text("الدعوة غير ممكنة"),
-                          content: Text("أكثر من العدد الأقصى المحدد للجمعية"),
-                          actions: [
-                            okButton,
-                          ],
-                        );
-
-                        // show the dialog
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return alert;
-                          },
-                        );
-                      }
-                      /*
-                      AlertDialog alert = AlertDialog(
-                        title: Text("My title"),
-                        content: Text("This is my message."),
-                        actions: <Widget>[
-                          TextButton(child: Text("Yes"),
-                          onPressed: null,),  
-                        ],
-                      );
-
-                      // show the dialog
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return alert;
-                        },
-                      );
-                    */
-                    }*/
-                                },
-                                child: Text("دعوة"))
+                                    Fluttertoast.showToast(
+                                      msg:
+                                          "قمت بدعوة  ${selectedEmails.length} من أصدفائك",
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      timeInSecForIosWeb: 1,
+                                      backgroundColor: Colors.black,
+                                      textColor: Colors.white,
+                                      fontSize: 16.0,
+                                    );
+                                  },
+                                  child: Text("دعوة")),
+                            ),
                           ],
                         ),
                       );
@@ -317,7 +267,6 @@ class _inviteFriendsState extends State<inviteFriends> {
                       child: CircularProgressIndicator(),
                     );
                   }),
-              //],
             ],
           ),
         ));
@@ -328,35 +277,27 @@ class _inviteFriendsState extends State<inviteFriends> {
     feature.forEach((element) {
       invitedAlready.add(element);
     });
-    //print('readinvited211');
-    ///print(invitedAlready);
 
     ///--------------------- this for max? if so no need
     final docInviteFriends = FirebaseFirestore.instance
         .collection('JamiaGroup')
         .doc(jamiaId)
         .collection('members');
-    //.doc(element);
-    //print('number');
     len = await docInviteFriends.snapshots().length;
-    //print(len);
   }
 
   Future<List<dynamic>> readInvited2() async {
     final querySnapshot = await FirebaseFirestore.instance
         .collection("JamiaGroup")
-        .doc(jamiaId) //chenge this to jamia id
+        .doc(jamiaId)
         .collection('members')
         .get();
-    //print('querey snapshot');
-    //print(querySnapshot.docs);
+
     List<dynamic> res = List<dynamic>.empty(growable: true);
-    //print('readinvited2');
+
     querySnapshot.docs.forEach((element) {
-      //print(element.id);
       res.add(element.id);
     });
-    //print(res);
     return res;
   }
 }
