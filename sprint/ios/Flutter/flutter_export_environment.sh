f354d45d85d3522f097b1aa2e2c57b9a7e7dbe94
AfnanSD <99503463+AfnanSD@users.noreply.github.com>
2022-10-20 01:41:56 +0300

    miss

--- sprint/ios/Flutter/flutter_export_environment.sh
+++ sprint/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\afnan\Desktop\Jamia-linaSprint3 (1)\Jamia-linaSprint3\sprint"
+export "FLUTTER_APPLICATION_PATH=C:\Users\afnan\Desktop\5th semester\SWE 444\Jamia\sprint"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
